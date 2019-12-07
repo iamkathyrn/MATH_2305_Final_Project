8933d90e3a82fc50f2851d210a3e91e833ff652e
Jeffrey-Tijerina <57689242+Jeffrey-Tijerina@users.noreply.github.com>
2019-12-06 19:52:14 -0600

    Add files via upload
    added imports, expanded prims

--- algorithms/prims_algorithm.py
+++ algorithms/prims_algorithm.py
@@ -1,10 +1,19 @@
+#prims goes here
+from functions.graph_operations import min_cost_incident_edge, initialize_tree
+from functions.reading_writing_functions import get_graph
 
-
-# prims goes here
 def prims(textfile):
     G = get_graph(textfile)
     T = initialize_tree(G, v)
     
     while T[0] != G[0]:
-        e = min_cost_edge(G, T)
-        # add e and its vertices to T
+        e = min_cost_incident_edge(G, T)
+        #e = min_cost_edge(G, T)
+        #add e and its verticies to T.....
+        #######################################
+        T[1].append(e)
+        T[0].append(e[0])
+        T[1].append(e[1])
+        #########################################
+        
+    return T
