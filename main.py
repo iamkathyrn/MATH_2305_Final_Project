1d22929535d2ba796b09e849eb47d8eb5691d54d
Jeffrey-Tijerina <57689242+Jeffrey-Tijerina@users.noreply.github.com>
2019-12-10 16:05:08 -0600

    updated calls and display
    updated calls and display of information on major changes made to graph_operations and prims_algorithms

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from algorithms import prims
-from functions.graph_operations import graph_cost
+from algorithms.prims_algorithms import prims
+from functions.reading_writing_function import get_graph
+from functions.graph_operations import *
 
+#MST - minimum spanning tree
 
-text = input('Please provide a graph to run the TSP on ')
-
-vt = input('Please provide a starting vertex ')
-
-T = prims(G, v)
-
-print('Optimal Tree is {T}')
-
+input_graph = get_graph('G1.txt') 
+start_node = int(input(f'Choose a starting node, {input_graph[0]}: ')) 
 print('')
 
-print('Optimal cost: {graph_cost(T)}')
+
+prims_MST = prims(input_graph, start_node)
+
+total_tree_weight = total_tree_weight(input_graph, prims_MST)
+
+vertices, edges = prims_MST
+
+###############################################################################
+#######################     PRINT RESULTS    ##################################
+      
+print('')
+print('Prim''s algorithm - MST - "minimum spanning tree"')
+print('')
+print('')
+print(f'MST NODES : {vertices}')
+print(f'MST EDGES : {edges}')
+print('')
+print(f'MST WEIGHT (cost): {total_tree_weight}')
--- test.py
+++ test.py
@@ -1,26 +1,25 @@
+import numpy as np
+from functions.reading_writing_function import get_graph
+from algorithms.prims_algorithms import prims
+from functions.graph_operations import *
 
-from functions.reading_writing_functions import get_graph
-from functions.graph_operations import incident_edges, cost
 
-    
-G = get_graph('G1.txt')    
 
-    
-print(f'V(G) = {G[0]}')
-print('')
-print(f'E(G) = {G[1]}')
+input_graph = get_graph('G1.txt') 
+start_node = int(input(f'Choose a starting node, {input_graph[0]}:')) 
 print('')
 
+prims_MST = prims(input_graph, start_node)
+total_tree_weight = total_tree_weight(input_graph, prims_MST)
+vertices, edges = prims_MST
 
-#initialize tree
+#[NODES],[(EDGES)]
+T =  ([0], [()])
 
-T = ([3, 1, 4], [(1, 3), (1, 4)])
-
-print(f' T = {T}')
 print('')
-
-#print(incident_edges(G, T))
-
-for e in incident_edges(G, T):
-    print(f'Edge {e} with cost {cost(G, e)}')
-
+print('Prim''s algorithm - "minimum spanning tree"')
+print('')
+print(f'MST NODES: {vertices}')
+print(f'MST EDGES : {edges}')
+print('')
+print(f'MST WEIGHT (cost): {total_tree_weight}')
