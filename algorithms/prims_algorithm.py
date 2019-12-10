f2d25ee7c4d56ce473b6646e16364a23c87dd47b
Jeffrey-Tijerina <57689242+Jeffrey-Tijerina@users.noreply.github.com>
2019-12-10 16:54:18 -0600

    minor changes
    minor changes - syntax errors - for loops

--- algorithms/prims_algorithm.py
+++ algorithms/prims_algorithm.py
@@ -10,17 +10,17 @@ def prims(input_graph, start_node):
     #while ((length (tree nodes)) != (length of the (graph nodes)))
     while (len(tree[0]) != len(input_graph[0])): 
         #loops to check all possible paths in tree p-(path)
-        for (p in all_known_paths):
-            for (node_1 in tree[0]):
-                for (node_2 in tree[0]):         
+        for p in all_known_paths:
+            for node_1 in tree[0]:
+                for node_2 in tree[0]:         
                     #comparing path values and append edge of smallest path found
                     if (p == min_cost_incident_edge(input_graph, tree)) and ( str(p) != '(' + str (node_1) + ', ' + str(node_2) + ')' ): 
                         tree[1].append(p)
                         
             #Loops through all nodes in tree e-(edge)
-            for (e in tree[1]):
+            for e in tree[1]:
                 #Loops and checks all nodes in edges
-                for (node in e):
+                for node in e:
                     #append node not in tree
                     if (node not in tree[0]):
                         tree[0].append(node)
